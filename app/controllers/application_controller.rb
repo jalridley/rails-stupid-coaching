3964307f5d9003e27505b2a7e5c9a907e7afd347
Jalynn Ridley <jallywally@gmail.com>
2021-05-17 15:50:54 +0200

    styling incomplete which i can do without TA help so moving on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
 end
+
+# First page is a form with an input, where a user can type a question to ask the Coach
+
+# After submitting the form, the user is redirected to another page where she/he will see her/his question and the coach answer.
